0077bc36694f409542503f22665daf33643eb7b3
Bryan J Swift <bryan.j.swift@gmail.com>
2012-11-26 16:31:56 -0500

    Fix readme and add some docs to puppet::install

---
+++ README.md
@@ -0,0 +1,16 @@
+# puppet
+
+Manage puppet environment configuration
+
+## License
+
+Apache License, Version 2.0
+
+## Contact
+
+Via email [bryan.j.swift@gmail.com](mailto:bryan.j.swift+puppet-puppet@gmail.com?subject=puppet-puppet)
+
+## Support
+
+Please log tickets and issues on [github](http://github.com/bryanjswift/puppet-puppet/issues)
+
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,23 @@
+# == Class: puppet::install
+#
+# Install the puppet package
+#
+# === Examples
+#
+#  include puppet::install
+#
+# === Authors
+#
+# Bryan J Swift <bryan.j.swift@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2012 Bryan J Swift
+#
 class puppet::install {
-  package { "puppet":
+
+  package { 'puppet':
     ensure => present,
   }
+
 }
