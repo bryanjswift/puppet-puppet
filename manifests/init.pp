61ea967dda3e6be96731c7d1fac92ce16a9a0128
Bryan J Swift <bryan.j.swift@gmail.com>
2012-11-27 12:09:48 -0500

    Fix some linting errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,45 @@
+# == Class: puppet
+#
+# Manage puppet configuration and service.
+#
+# === Parameters
+#
+# [*environment*]
+#   Set the environment for the puppet agent
+#   Default: production
+#
+# [*server*]
+#   The server to connect to as the puppetmaster
+#   Default: puppet
+#
+# [*start*]
+#   Whether or not the daemon should be started on startup (yes|no).
+#   Default: yes
+#
+# === Examples
+#
+#  include puppet
+#
+# === Authors
+#
+# Bryan J Swift <bryan.j.swift@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2012 Bryan J Swift
+#
 class puppet(
-  $environment = "production",
-  $server      = "puppet",
-  $start       = "yes",
+  $environment = 'production',
+  $server      = 'puppet',
+  $start       = 'yes',
 ) {
 
-  if ($environment == "") {
-    fail("environment must be defined")
+  if ($environment == '') {
+    fail('environment must be defined')
   }
 
-  if ($server == "") {
-    fail("server must be defined")
+  if ($server == '') {
+    fail('server must be defined')
   }
 
   validate_re($start, '^(yes|no)$', 'The $start value must be yes or no.')
