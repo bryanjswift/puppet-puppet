40021e80ba263eb3ca0b319686cbae1652fac890
Bryan J Swift <bryan.j.swift@gmail.com>
2012-11-27 12:44:10 -0500

    Add comments and fix linting errors

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,26 +1,40 @@
+# == Class: puppet::config
+#
+# Manage configuration for puppet service
+#
+# [ps]: https://github.com/pdxcat/puppet-sync
+#
+# === Authors
+#
+# Bryan J Swift <bryan.j.swift@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2012 Bryan J Swift
+#
 class puppet::config {
 
   $puppetserver = $puppet::server
   $puppetenv    = $puppet::environment
   $puppetstart  = $puppet::start
 
-  file { "/etc/puppet/puppet.conf":
+  file { '/etc/puppet/puppet.conf':
     ensure  => present,
-    content => template("puppet/puppet.conf.erb"),
-    owner   => "puppet",
-    group   => "puppet",
-    require => [Class["puppet::install"]],
-    notify  => [Class["puppet::service"]],
+    content => template('puppet/puppet.conf.erb'),
+    owner   => 'puppet',
+    group   => 'puppet',
+    require => [Class['puppet::install']],
+    notify  => [Class['puppet::service']],
   }
 
-  file { "/etc/default/puppet":
+  file { '/etc/default/puppet':
     ensure  => present,
-    content => template("puppet/etc.default.puppet.erb"),
-    owner   => "root",
-    group   => "root",
-    mode    => 0644,
-    require => [Class["puppet::install"]],
-    notify  => [Class["puppet::service"]],
+    content => template('puppet/etc.default.puppet.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => [Class['puppet::install']],
+    notify  => [Class['puppet::service']],
   }
 
 }
